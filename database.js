33f3db1a14ae9f35f512370eab32bd80c8268fb7
sagarp26 <sagarp26@ad.unc.edu>
2022-04-11 13:23:38 -0400

    Updated code

--- database.js
+++ database.js
@@ -1,28 +1,18 @@
-"use strict"
+const database = require('better-sqlite3')
 
-const Database = require("better-sqlite3")
+const logdb = new database('log.db')
 
-const db = new Database("log.db")
+const stmt = logdb.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name='accesslog';`)
+let row = stmt.get();
 
-const stmt = db.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name='accesslog';`)
-let row = stmt.get()
-if (row === undefined) {
-    const sqlInit = `
-        CREATE TABLE accesslog (
-            id INTEGER PRIMARY KEY,
-            remoteaddr TEXT,
-            remoteuser TEXT,
-            time TEXT,
-            method TEXT
-            url TEXT,
-            protocol TEXT,
-            httpversion TEXT,
-            status TEXT,
-            referer TEXT,
-            useragent TEXT
-        );
-    `
-    db.exec(sqlInit)
+if(row === undefined) {
+    console.log('Log database missing')
+
+    const sqlInit = `CREATE TABLE accesslog ( id INTEGER NOT NULL PRIMARY KEY, remoteaddr TEXT, remoteuser TEXT, time INTEGER, method TEXT, url TEXT, protocol TEXT, httpversion TEXT, status INTEGER, referer TEXT, useragent TEXT);`;
+
+    logdb.exec(sqlInit)
+} else {
+    console.log('Log database exists.')
 }
 
-module.exports = db
+module.exports = logdb
--- server.js
+++ server.js
@@ -1,41 +1,73 @@
-const express = require("express")
+const express = require('express')
 const app = express()
-const fs = require("fs")
-const db = require("./database.js")
-const morgan = require("morgan")
 
-const args = require("minimist")(process.argv.slice(2))
+const morgan = require('morgan')
+
+const fs = require('fs')
+
+const logdb = require('./database.js')
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+const args = require('minimist')(process.argv.slice(2))
+
 const port = args.port || process.env.PORT || 5555
-const help = (`
-server.js [options]
---port  Set the port number for the server to lister on. Must be an interger
-            between 1 and 65535.
---debug If set to true, creates endpoints /app/log/access/ which returns
-            a JSON access log from the database and /app/error/ which throws
-            an error with the message "Error test successful." Defaults to
-            false.
---log           If set to false, no log files are written. Defaults to true.
-            Logs are always written to database.
---help  Return this message and exit.
-`)
 
 const server = app.listen(port, () => {
-  console.log("App listening on port %PORT%".replace("%PORT%", port))
+  console.log('App listening on port %PORT%'.replace('%PORT%', port))
 })
 
-if (args.help || args.h) {
+const help = (`
+server.js [options]
+--port	Set the port number for the server to listen on. Must be an integer
+            between 1 and 65535.
+--debug	If set to true, creates endlpoints /app/log/access/ which returns
+            a JSON access log from the database and /app/error which throws 
+            an error with the message "Error test successful." Defaults to 
+            false.
+--log		If set to false, no log files are written. Defaults to true.
+            Logs are always written to database.
+--help	Return this message and exit.
+`)
+
+if(args.help || args.h) {
   console.log(help)
   process.exit(0)
 }
 
-if (args.debug) {
-  app.get("/app/log/access", (req, res) => {
-    const stmt = db.prepare("SELECT * FROM accesslog").all()
-    res.status(200).json(stmt)
-  })
+if(args.log != false) {
+    const accesslog = fs.createWriteStream('access.log', { flags: 'a'})
+    app.use(morgan('combined', {stream: accesslog}))
+}
 
-  app.get("/app/error", (req, res) => {
-    throw new Error("Error test successful.")
+app.use((req, res, next) => {
+    let logdata = {
+      remoteaddr: req.ip,
+      remoteuser: req.user,
+      time: Date.now(),
+      method: req.method,
+      url: req.url,
+      protocol: req.protocol,
+      httpversion: req.httpVersion,
+      status: res.statusCode,
+      referer: req.headers['referer'],
+      useragent: req.headers['user-agent']
+    }
+
+    const stmt = logdb.prepare('INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)')
+    const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referer, logdata.useragent)
+    
+    next()
+})
+
+if(args.debug) {
+  app.get('app/log/access', (req, res) => {
+      const stmt = logdb.prepare('SELECT * FROM accesslog').all()
+      res.status(200).json(stmt)
+  })
+  app.get('/app/error', (req, res) => {
+    throw new Error("Error test successful")
   })
 }
 
@@ -64,29 +96,6 @@ app.get("/app/flip/call/tails/", (req, res) => {
   res.setHeader("Content-Type", "text/json")
   res.status(200).json(flipACoin("tails"))
 })
-
-app.use((req, res, next) => {
-  let logdata = {
-    remoteaddr: req.ip,
-    remoteuser: req.user,
-    time: Date.now(),
-    method: req.method,
-    url: req.url,
-    protocol: req.protocol,
-    httpversion: req.httpVersion,
-    status: res.statusCode,
-    referer: req.headers['referer'],
-    useragent: req.headers['user-agent']
-  }
-  const stmt = db.prepare("INSERT INTO accesslog (remoteaddr, remoteuser, time, method, url, protocol, httpversion, status, referer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)")
-  const info = stmt.run(logdata.remoteaddr, logdata.remoteuser, logdata.time, logdata.method, logdata.url, logdata.protocol, logdata.httpversion, logdata.status, logdata.referer, logdata.useragent)
-  next()
-})
-
-if (args.log != false) {
-  const WRITESTREAM = fs.createWriteStream("access.log", {flags: 'a'})
-  app.use(morgan("combined", {stream: WRITESTREAM}))
-}
 
 app.use(function(req, res) {
   res.status(404).send("404 NOT FOUND")
